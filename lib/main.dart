645b03765346174e6a787d3ab22d5ea4b2084612
skedaddl3 <hexisdylan23@gmail.com>
2022-07-04 23:06:25 +0800

    main act

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 void main() {
   runApp(
@@ -23,7 +26,55 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Montfort Academy - Student Portal',
             style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+        centerTitle: true,
       ),
+      // ignore: prefer_const_constructors
+      body: const LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle defaultStyle = const TextStyle(color: Colors.black, fontSize: 15);
+    TextStyle linkStyle = const TextStyle(color: Colors.blue);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+            child: Image.asset('assets/images/Montfort-removebg-preview.png'),
+            width: 220),
+        const Text("\n\n"),
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: defaultStyle,
+            children: <TextSpan>[
+              const TextSpan(text: 'Currently Enrolled Student? '),
+              TextSpan(
+                  text: 'Login Here',
+                  style: linkStyle,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      print('Terms of Service"');
+                    }),
+              const TextSpan(text: '\n\nHaving Troubles Logging In? '),
+              TextSpan(
+                  text: 'Create an account or Report a Problem',
+                  style: linkStyle,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      print('Privacy Policy"');
+                    }),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
